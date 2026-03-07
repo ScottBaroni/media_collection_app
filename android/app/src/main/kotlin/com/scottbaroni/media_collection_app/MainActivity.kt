8ec0d61de5f2f62a8ab3409d0f29e66a2b90cf11
Scott <sbaroni2004@gmail.com>
2026-03-06 18:39:46 -0800

    Initial setup: models and provider skeleton

---
+++ android/app/src/main/kotlin/com/scottbaroni/media_collection_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.scottbaroni.media_collection_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/collection_type.dart
@@ -0,0 +1,23 @@
+// Collection Type
+
+import 'package:media_collection_app/models/media_item.dart';
+
+class CollectionType {
+  final String id;
+  final String name;
+  final String iconName;
+  final bool isCustom; // Default or custom collection
+
+  const CollectionType({
+    required this.id,
+    required this.name,
+    required this.iconName,
+    this.isCustom = false,
+  });
+
+  static const List<CollectionType> defaults = [
+    CollectionType(id: 'cds',     name: 'CDs',    iconName: 'album'),
+    CollectionType(id: 'dvds',    name: 'DVDs',   iconName: 'movie'),
+    CollectionType(id: 'books',   name: 'Books',  iconName: 'menu_book'),
+  ];
+}
