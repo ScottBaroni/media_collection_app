09246257af6bbeef06e771e39da6c090059c3c9d
Scott <sbaroni2004@gmail.com>
2026-03-08 19:31:51 -0700

    Added navigation bar (nav shell)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/collection_provider.dart';
+import 'screens/shell_screen.dart';
 
 void main() {
   runApp(const MediaShelfApp());
@@ -21,7 +22,7 @@ class MediaShelfApp extends StatelessWidget {
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
-        home: const Placeholder(), // Replace this with the shell later
+        home: const ShellScreen(),
       ),
     );
   }
---
+++ lib/screens/shell_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ShellScreen extends StatefulWidget {
+  const ShellScreen({super.key});
+
+  @override
+  State<ShellScreen> createState() => _ShellScreenState();
+}
+
+class _ShellScreenState extends State<ShellScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const Placeholder(), // Collection
+    const Placeholder(), // Stats
+    const Placeholder(), // Profile
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grid_view_rounded),
+            label: 'Collection',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_rounded),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_rounded),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
